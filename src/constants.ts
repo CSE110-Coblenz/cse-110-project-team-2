4c44a09fd76bca01e56970b03d65b5a91f14853e
michiisai <mim019@ucsd.edu>
2025-11-04 13:34:13 -0800

    updated constants

--- src/constants.ts
+++ src/constants.ts
@@ -17,9 +17,4 @@ export type ToppingType='Mushroom'|'Pepper'|'Pepperoni';
 export const PIZZA ={ R_OUTER:200};
 export const SLICE_OPTIONS=[4,8,12,16] as const;
 
-<<<<<<< HEAD
 export const MINIGAME2_DURATION = 30;
-=======
-export const MINIGAME2_DURATION = 30;
-
->>>>>>> 17610f1 (changed game duration to global constant)
